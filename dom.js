462b8691ec9697c2135da1c00c4c299f77198049
dan <dandan682@outlook.com>
2021-05-20 01:22:30 -0500

    update  dom

--- dom.js
+++ dom.js
@@ -5,12 +5,43 @@ console.log(document.body)
 console.log(document.domain)
 console.log(document.all)
 console.log(document.forms)
-let headerTitle = document.getElementById('header-title')
-let header = document.getElementById("main-header");
-console.log(headerTitle)
+// let headerTitle = document.getElementById('header-title')
+// let header = document.getElementById("main-header");
+// console.log(headerTitle)
 // headerTitle.innerText = "Goodbye"
-console.log(headerTitle.textContent);
-console.log(headerTitle.innerText);
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
 // headerTitle.innerHTML = "<h3>Adios</h3>"
 // headerTitle.style.borderBottom = 'solid 3px #000'
-header.style.borderBottom = 'solid 3px #000'
+// header.style.borderBottom = 'solid 3px #000'
+
+// QUERYSELECTOR
+
+// let header = document.querySelector('#main-header') 
+// header.style.borderBottom = 'solid 4px #ccc'
+// let input = document.querySelector("input") 
+// input.value = 'Hello World'
+// let submit = document.querySelector('input[type="submit"]') 
+// submit.value = 'SEND'
+// let item = document.querySelector('.list-group-item')
+// item.style.color = 'red'
+// let lastItem = document.querySelector(".list-group-item:last-child")
+// lastItem.style.color = "blue"
+// let secondItem = document.querySelector(".list-group-item:nth-child(2")
+// secondItem.style.color = "coral"
+
+//QUERYSELECTORALL
+
+let titles = document.querySelectorAll('.title')
+console.log(titles)
+
+let odd = document.querySelectorAll("li:nth-child(odd)")
+let even = document.querySelectorAll('li:nth-child(even)')
+
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "#f4f4f4"
+    even[i].style.backgroundColor = '#ccc'
+}
+
+
+
