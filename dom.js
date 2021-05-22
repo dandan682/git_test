d986df81cc6adebf624c958b5228b602848c0834
dan <dandan682@outlook.com>
2021-05-21 20:37:33 -0500

    update dom.js

--- dom.js
+++ dom.js
@@ -5,6 +5,7 @@ console.log(document.body)
 console.log(document.domain)
 console.log(document.all)
 console.log(document.forms)
+
 // let headerTitle = document.getElementById('header-title')
 // let header = document.getElementById("main-header");
 // console.log(headerTitle)
