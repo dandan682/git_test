dfa46012d64749dd5cd550cc2a3f93d352f42b15
dan <dandan682@outlook.com>
2021-05-23 22:32:12 -0500

    update dom.js

--- dom.js
+++ dom.js
@@ -60,3 +60,22 @@ console.log(itemList.lastElementChild)
 itemList.lastElementChild.textContent = 'Hello 4'
 
 itemList.previousElementSibling.style.color = "green";
+
+// Create a div
+let newDiv = document.createElement('div')
+// Add class
+newDiv.className = 'hello'
+// Add id
+newDiv.id = 'hello1'
+// Add attr
+newDiv.setAttribute('title', 'Hello div')
+// Create text node
+let newDivText = document.createTextNode('Hello World')
+// Add text to div
+newDiv.appendChild(newDivText)
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1')
+
+console.log(newDiv)
+newDiv.style.fontSize = '30px'
+container.insertBefore(newDiv, h1)
