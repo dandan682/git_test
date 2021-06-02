68ceaf6cc5fc149319523fc68362887fe63ce7f5
dan <dandan682@outlook.com>
2021-06-02 17:21:23 -0500

    update toDoApp.js

--- toDoApp.js
+++ toDoApp.js
@@ -1,45 +1,67 @@
-let form = document.getElementById("addForm");
-let itemList = document.getElementById("items");
-let filter = document.getElementById("filter");
-let item = document.getElementById("item");
+let filter = document.querySelector(".search");
+let form = document.querySelector(".addItem");
+let itemInput = document.querySelector(".item-input");
+let container = document.querySelector(".container");
 
-
-form.addEventListener("submit", addItem)        // Form submit event
-itemList.addEventListener("click", removeItem)  // Delete event
 filter.addEventListener("keyup", filterItems)     // Filter event
+form.addEventListener("submit", addItem)        // Form submit event
 
-// Add task
+
+// Add item
 function addItem(e) {
   e.preventDefault();
-  let newItem = document.getElementById("item").value; // Get input value
-  let li = document.createElement("li")         // Create new li element
-  li.className = "list-group-item"              // Add class
-  li.appendChild(document.createTextNode(newItem))     // Add text node with input value
+  //  let newItem = document.querySelector(".itemInput").value; // Get input value
 
-  let deleteBtn = document.createElement("button")     // Create del button element
-  deleteBtn.className = "delete"; // Add classes to del button
-  deleteBtn.appendChild(document.createTextNode("X")); // Append text node
-  li.appendChild(deleteBtn); // Append button to li
-  itemList.appendChild(li); // Append li to list
-  item.value = ""; // Clear input value
+  let div = document.createElement("div"); // Create new div  element
+  div.className = "items"; // Add class
+
+  let input = document.createElement("input"); // Create del button element
+  input.className = "item"; // Add classes to del button
+  input.value = itemInput.value; // Append text node
+  input.type = "text";
+  input.disabled = true;
+
+  let editBtn = document.createElement("button"); // Create del button element
+  editBtn.className = "button edit"; // Add classes to del button
+  editBtn.innerHTML = "EDIT"; // Append text node
+
+  let deleteBtn = document.createElement("button"); // Create del button element
+  deleteBtn.className = "button delete"; // Add classes to del button
+  deleteBtn.innerHTML = "X"; // Append text node
+
+  container.appendChild(div); // Append div to container
+  console.log(container);
+
+  div.appendChild(input); // Append button to div
+  div.appendChild(editBtn); // Append button to div
+  div.appendChild(deleteBtn); // Append button to div
+
+  editBtn.addEventListener("click", editItem(input)); // Delete event
+  deleteBtn.addEventListener("click", removeItem); // Delete event
+
+  itemInput.value = ""; // Clear input item value
 }
 
+function editItem(e) {
+  e.disabled = !e.disabled
+}
 // Remove item
 function removeItem(e) {
+  console.log("remove")
   if (e.target.classList.contains("delete")) {
     if (confirm("Are You Sure?")) {
-      let li = e.target.parentElement;
-      itemList.removeChild(li);
+      let div = e.target.parentElement;
+      container.removeChild(div);
     }
   }
 }
 
 // Filter Items
 function filterItems(e) {
-  // convert text to lowercase
-  let text = e.target.value.toLowerCase();
-  // Get lis
-  let items = itemList.getElementsByTagName("li");
+  let text = e.target.value.toLowerCase(); // convert text to lowercase
+  let items = container.querySelector(".items"); // Get divs
+  console.log(items);
+
   // Convert to an array
   Array.from(items).forEach(function (item) {
     let itemName = item.firstChild.textContent;
